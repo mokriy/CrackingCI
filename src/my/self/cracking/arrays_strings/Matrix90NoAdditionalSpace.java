add40a9a01ca9a1aea0a5d9b8e593e77b5abace2
Oleksiy Mokrynskiy <oleksiy.mokrynskiy@booxware.de>
2019-05-07 16:34:00 +0200

    Cosmetics

--- src/my/self/cracking/arrays_strings/Matrix90NoAdditionalSpace.java
+++ src/my/self/cracking/arrays_strings/Matrix90NoAdditionalSpace.java
@@ -4,25 +4,27 @@ public class Matrix90NoAdditionalSpace {
     static byte[][] image = new byte[4][4];
 
     public static void main(String[] args) {
-        for (byte row = 0; row < 4; row ++) {
-            for (byte col = 0; col < 4; col ++) {
-                image[row][col] = (byte) (row*10 + col);
+        for (byte row = 0; row < 4; row++) {
+            for (byte col = 0; col < 4; col++) {
+                image[row][col] = (byte) (row * 10 + col);
             }
         }
 
         System.out.println("Before rotation: ");
 
-        printMatrix(image);
+        printMatrix();
 
         System.out.println("After rotation: ");
 
-        printMatrix(rotate90());
+        rotate90();
+
+        printMatrix();
     }
 
-    private static void printMatrix(byte[][] toRotate) {
-        for (byte row = 0; row < 4; row ++) {
-            for (byte col = 0; col < 4; col ++) {
-                System.out.print(toRotate[row][col]);
+    private static void printMatrix() {
+        for (byte row = 0; row < 4; row++) {
+            for (byte col = 0; col < 4; col++) {
+                System.out.print(image[row][col]);
                 if (col < 3) {
                     System.out.print(",");
                 }
@@ -31,22 +33,21 @@ public class Matrix90NoAdditionalSpace {
         }
     }
 
-    private static byte[][] rotate90() {
-        for (int i = 0; i < 3; i ++) {
+    private static void rotate90() {
+        for (int i = 0; i < 3; i++) {
             rotationStep(image[0][i], 0, i, 0);
         }
         rotationStep(image[1][1], 1, 1, 0);
-        return image;
     }
 
     private static void rotationStep(byte value, int row, int col, int hopCounter) {
-       byte nextVal = image[col][3 - row];
-       image[col][3 - row] = value;
-       int newCol = 3 - row;
-       if (hopCounter == 3) {
-           image[col][newCol] = value;
-       } else {
-           rotationStep(nextVal, col, newCol, hopCounter + 1);
-       }
+        byte nextVal = image[col][3 - row];
+        image[col][3 - row] = value;
+        int newCol = 3 - row;
+        if (hopCounter == 3) {
+            image[col][newCol] = value;
+        } else {
+            rotationStep(nextVal, col, newCol, hopCounter + 1);
+        }
     }
 }
